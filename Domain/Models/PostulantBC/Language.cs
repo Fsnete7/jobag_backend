f4c4ca89e16eef38658d0c2201f0b77005f0a16d
Fsnete <43191130+Fsnete@users.noreply.github.com>
2021-10-21 22:49:35 -0500

    feat:"Postulant Bounded Context Models"

---
+++ Domain/Models/PostulantBC/Language.cs
@@ -0,0 +1,11 @@
+﻿namespace jobagapi.Domain.Models.PostulantBC
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+        
+    }
+}
