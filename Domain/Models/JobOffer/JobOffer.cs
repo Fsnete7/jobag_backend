7998f97d661b8341dda6e91d9fce8f7171f773c8
eduperaltas <61795886+eduperaltas@users.noreply.github.com>
2021-10-23 09:40:08 -0500

    jobOffer v1
    jobOffer Bounded Context v1

---
+++ Domain/Models/JobOffer/JobOffer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace jobagapi.Domain.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Salary { get; set; }
+        public string Description { get; set; }
+        public string Workplace { get; set; }
+        public DateTime Date { get; set; }
+        public string Experience { get; set; }
+    }
+}
---
+++ Services/JobOffer/JobOfferService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using jobagapi.Domain.Models;
+using jobagapi.Domain.Repositories;
+using jobagapi.Domain.Services;
+
+namespace jobagapi.Services
+{
+    public class JobOfferService : IJobOfferService
+    {
+        private readonly IJobOfferRepository _jobOfferRepository;
+
+        public JobOfferService(IJobOfferRepository jobOfferRepository)
+        {
+            _jobOfferRepository = jobOfferRepository;
+        }
+
+        public async Task<IEnumerable<JobOffer>> ListAsync()
+        {
+            return await _jobOfferRepository.ListAsync();
+        }
+    }
+}
