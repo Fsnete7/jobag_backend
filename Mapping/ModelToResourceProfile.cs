5df0f130567a22fc8f2140bc12b7558ee798071c
Fsnete <43191130+Fsnete@users.noreply.github.com>
2021-11-18 13:32:43 -0500

    Fix

--- Mapping/ModelToResourceProfile.cs
+++ Mapping/ModelToResourceProfile.cs
@@ -3,6 +3,7 @@ using jobagapi.Domain.Models.EmployerSystem;
 using jobagapi.Domain.Models.JobOfferSystem;
 using jobagapi.Domain.Models.PostulantSystem;
 using jobagapi.Domain.Models.SubscriptionSystem;
+using jobagapi.Resources.EmployerResources;
 using jobagapi.Resources.JobOfferResources;
 using jobagapi.Resources.PostulantResources;
 using jobagapi.Resources.SubscriptionResources;
@@ -15,15 +16,15 @@ namespace jobagapi.Mapping
         public ModelToResourceProfile()
         {
 
-            //--------------- Scoped Subscription Bounded Context ---------------
+            //--------------- Scoped Postulant Bounded Context ---------------
             CreateMap<JobOffer, jobOfferResource>();
             CreateMap<Postulant, PostulantResource>();
-            
             CreateMap<ProfessionalProfile, ProfessionalProfileResource>();
             CreateMap<Language, LanguageResource>();
             CreateMap<Skill, SkillResource>();
             CreateMap<Degree, DegreeResource>();
 
+            //--------------- Scoped Employer Bounded Context ---------------
             CreateMap<Employer, EmployerResource>();
             
             //--------------- Scoped Subscription Bounded Context ---------------
